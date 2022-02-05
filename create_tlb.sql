5220a604c24e1d5eb48ad0707527940a603c5df8
kench1994 <kench.h.wong@gmail.com>
2022-02-05 20:52:15 +0800

    create tblCvtBond.sql

--- create_tlb.sql
+++ create_tlb.sql
@@ -14,3 +14,18 @@ CREATE TABLE IF NOT EXISTS `tblIPOStock`(
    `中签率` DOUBLE comment '%',
    PRIMARY KEY ( `股票代码` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `tblCvtBond`(
+   `债券代码` CHAR(10) NOT NULL,
+   `债券简称` CHAR(10) NOT NULL,
+   `申购日期` DATE DEFAULT NULL,
+   `申购代码` CHAR(10) NOT NULL,
+   `申购上限` DOUBLE comment '万元',
+   `股权登记日` DATE DEFAULT NULL,
+   `每股配售额` DOUBLE,
+   `发行规模` DOUBLE,
+   `中签号发布日` DATE DEFAULT NULL,
+   `中签率` DOUBLE,
+   `上市时间` DATE DEFAULT NULL,
+   PRIMARY KEY ( `债券代码` )
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
